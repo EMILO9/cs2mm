91b6b66fe75fe2f93281a5b523790dc465592db5
EMILO9 <emilberolsen@gmail.com>
2023-12-23 05:14:06 +0100

    d

--- initializers/appMiddlewares.js
+++ initializers/appMiddlewares.js
@@ -2,7 +2,7 @@ const helmet = require("helmet"),
   cors = require("cors");
 module.exports = (app, express) => {
   app.use(helmet());
-  app.use(cors({ origin: process.env.CORS_ORIGIN }));
+  app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 };
