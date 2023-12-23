19bbe58129790f6adb57af22f6d0e13ee8d848eb
EMILO9 <emilberolsen@gmail.com>
2023-12-23 04:48:18 +0100

    added a few modules

--- initializers/appMiddlewares.js
+++ initializers/appMiddlewares.js
@@ -1,5 +1,8 @@
+const helmet = require("helmet"),
+  cors = require("cors");
 module.exports = (app, express) => {
+  app.use(helmet());
+  app.use(cors({ origin: process.env.CORS_ORIGIN }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static("public"));
 };
