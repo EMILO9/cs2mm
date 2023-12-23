fe01db1638cb6a1bfd654923d8e9c589a3b63f00
EMILO9 <emilberolsen@gmail.com>
2023-12-23 03:37:47 +0100

    d

--- initializers/404Handler.js
+++ initializers/404Handler.js
@@ -1,5 +1,7 @@
 module.exports = (app) => {
   app.use((req, res) => {
-    res.status(404).send({ error: "API not found." });
+    res
+      .status(404)
+      .send({ error: "The endpoint you are trying to access does not exist." });
   });
 };
--- loggers/devLogger.js
+++ loggers/devLogger.js
@@ -1,9 +1,16 @@
 const winston = require("winston");
+const { combine, timestamp, printf, colorize } = winston.format;
 module.exports = winston.createLogger({
   level: "debug",
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.simple()
+  format: combine(
+    timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    colorize(),
+    printf(
+      (info) =>
+        `${info.timestamp} ${info.level} [${info.service}]: ${info.message}`
+    )
   ),
   defaultMeta: { service: "dev.logger" },
   transports: [new winston.transports.Console()],
